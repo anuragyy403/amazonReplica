b4dfaae1fcfec3aa37d5ab6053d3a6ec9c423810
anuragyy403 <anuragysinghyadavv@gmail.com>
2026-07-13 00:56:33 +0530

    Pushing the project on the Github

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const protect = async (req,res,next) => {
+    let token = req.headers.authoristaion?
+    req.headers.authoristaion.split(' ')[1]
+    :null;
+
+    if(!token){
+        res.status(401).json({message: "Not authorised, no token"});
+        
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_secret);
+        req.user = await User.findById(decoded.id).select('-password');
+        next();
+    }catch (err) {
+        res.status(401).json({message: "Not authorised, token failed"});
+    };    
+    }
+};
+
+const admin = (req,res,next) => {
+    if(req.user && req.user.role === 'admin'){
+        next();
+    } else {
+        res.status(403).json({message: "Not authorised as an admin"});
+    }
+}
+
+module.exports = {protect, admin};
---
+++ server/models/product.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: [{
+        type: String,
+        required: true
+    }],
+    category: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'User',
+        required: true
+    },
+}, {timestamps: true});
+
+module.exports = mongoose.model('Product', productSchema);
